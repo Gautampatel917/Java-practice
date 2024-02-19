9cbacb0e11c7c7f4cdef2183dc364486b2fb98ee
Gautampatel917 <118241295+Gautampatel917@users.noreply.github.com>
2024-02-19 11:29:26 +0530

    Update BankDeposit.java

--- BankDeposit.java
+++ BankDeposit.java
@@ -1,24 +1,22 @@
-/* 
-BankDeposit
-1> openAccount : Account number, Holder name, Amount of deposit, Account type (Savings/Current)
-2> Show Account : Show the detail of account holder
-3> Deposit : We are merge this function with open account function
-4> Withdraw: Credit and Deposit amount
-*/
 
+/* BankDeposit
+ * 1> openAccount : Account number, Holder name, Amount of deposit, Account  type (Savings/Current)
+ * 2> Show Account : Show the detail of account holder
+ * 3> Deposit : We are merge this function with open account function
+ * 4> Withdraw: Credit and Deposit amount
+ */
 import java.util.*;
 
 class BankManagement {
-    String accountNumber, holderName, accountType;
+    String accountNumber, hldrNm, accountType;
     int amount = 0;
 
-    public void openAccount(String accNo, String holderName, int amount, char accType) {
+    public void openAccount(String accNo, String hldrNm, int amount, char accType) {
         this.accountNumber = accNo;
-        this.holderName = holderName;
+        this.hldrNm = hldrNm;
         this.amount = amount;
-
         if (accType == 's') {
-            accountType = "Savings";
+            accountType = "Saving";
         } else if (accType == 'c') {
             accountType = "Current";
         } else {
@@ -28,7 +26,7 @@ class BankManagement {
 
     public void show() {
         System.out.println("Account number: " + accountNumber);
-        System.out.println("Account holder: " + holderName);
+        System.out.println("Account holder: " + hldrNm.toUpperCase());
         System.out.println("Current amount: " + amount);
         System.out.println("Account type: " + accountType);
     }
@@ -52,50 +50,55 @@ class BankManagement {
 }
 
 public class BankDeposit {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner input = new Scanner(System.in);
-        String accountNumber, holderName;
-        char accountType;
+        Scanner inp = new Scanner(System.in);
+        String accountNumber, hldrNm;
+        char accType;
         int amount = 0;
-        BankManagement bank = new BankManagement();
+
+        BankManagement bnk = new BankManagement();
 
         System.out.print("Enter Account Number: ");
-        accountNumber = input.next();
+        accountNumber = inp.next();
 
-        System.out.print("Enter Holder Name: ");
-        holderName = input.next();
+        System.out.println("Enter Holder Name: ");
+        hldrNm = inp.next();
 
-        System.out.print("Enter amount of deposit: ");
-        amount = input.nextInt();
+        System.out.println("Enter amount of deposit: ");
+        amount = inp.nextInt();
 
-        System.out.print("Enter Account Types [Savings]/c[current]: ");
-        accountType = input.next().charAt(0);
+        System.out.print("Enter Account Types[Savings]/c[current]: ");
+        accType = inp.next().charAt(0);
 
-        bank.openAccount(accountNumber, holderName, amount, accountType);
+        bnk.openAccount(accountNumber, hldrNm, amount, accType);
         System.out.println("Account opened successfully!");
 
         char choice;
-        System.out.print("Do you want to withdrawal or deposit? y[yes] or n[no]: ");
-        choice = input.next().charAt(0);
+        System.out.println("Do you want to withdrawal or deposit? y[yes] or n[no]:");
+        choice = inp.next().charAt(0);
 
         if (choice == 'y') {
-            System.out.print("w[withdrawal] or d[deposit]: ");
-            char decision = input.next().charAt(0);
+            System.out.println("w[withdrawal] or d[deposit]: ");
+            char decision = inp.next().charAt(0);
 
-            System.out.print("Enter amount: ");
-            int amt = input.nextInt();
+            System.out.println("Enter amount: ");
+
+            int amt = inp.nextInt();
 
             if (decision == 'w') {
-                bank.withdrawal(amt);
+                bnk.withdrawal(amt);
             } else if (decision == 'd') {
-                bank.deposit(amt);
+                bnk.deposit(amt);
             } else {
                 System.out.println("Invalid option!!!");
             }
-        } else if (choice == 'n') {
-            System.out.println("Thank you for trusting us :)");
-        } else {
+
+        } 
+        else if (choice == 'n') {
+            System.out.println("Thank you for your trusting :)");
+        } 
+        else {
             System.out.println("Invalid option!!!");
         }
     }
